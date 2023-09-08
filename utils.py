0269e0f0d2c0c2465ea90476c36d45d43deae2b1
Hassan <hasan-account2012-3@live.com>
2023-09-08 18:04:08 +0200

    move notebooks to own folder, extract more methods to utils

--- utils.py
+++ utils.py
@@ -1,13 +1,19 @@
-from pathlib import Path
-from datetime import datetime
-from deepsvg.svglib.utils import to_gif
 import IPython.display as ipd
 import cairosvg
 import io
 import os
+import wandb
 import torch
-from dataset.dataset import decode
+from torch.optim import AdamW
+from datetime import datetime
+from pathlib import Path
+from deepsvg import utils
+from deepsvg.svglib.utils import to_gif
 from PIL import Image
+from dataset.dataset import decode
+from diffusion import create_diffusion
+from svgfusion import DiT
+
 
 def draw(svg_obj, fill=False, file_path=None, do_display=True, return_png=False,
          with_points=False, with_handles=False, with_bboxes=False, with_markers=False, color_firstlast=False,
@@ -40,18 +46,6 @@ def draw(svg_obj, fill=False, file_path=None, do_display=True, return_png=False,
             else:
                 return Image.open(file_path)
 
-def log_training(epoch_number: int, loss: float, timestep: int = None):
-    Path("./artifacts/").mkdir(parents=True, exist_ok=True)
-
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-
-    f = open("artifacts/log.txt", "a")
-
-    if timestep: f.write(f"{current_time} Epoch {epoch_number}: {loss} for timestep {timestep} \n\n")
-    else: f.write(f"{current_time} Epoch {epoch_number}: {loss} \n\n")
-
-    f.close()
 
 
 def sample_from_diffusion(vae_model, diffusion, model, class_labels, x_t=None, normalization_factor=0.7, display_gif=False, cfg_scale=4):
@@ -99,3 +93,136 @@ def sample_from_diffusion(vae_model, diffusion, model, class_labels, x_t=None, n
       decode((samples.unsqueeze(dim=0) / samples.std()) * normalization_factor, vae_model,) # * normalization_factor
 
       return samples
+
+def log_training(epoch_number: int, loss: float, timestep: int = None):
+    Path("./artifacts/").mkdir(parents=True, exist_ok=True)
+
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    f = open("artifacts/log.txt", "a")
+
+    if timestep: f.write(f"{current_time} Epoch {epoch_number}: {loss} for timestep {timestep} \n\n")
+    else: f.write(f"{current_time} Epoch {epoch_number}: {loss} \n\n")
+
+    f.close()
+
+def create_model(predict_xstart=True, dropout=0.1, n_classes=56, depth=28, learn_sigma=True, num_heads=16):
+
+    model = DiT(class_dropout_prob=dropout, num_classes=n_classes, depth=depth, learn_sigma=learn_sigma, num_heads=num_heads)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model.to(device)
+    diffusion = create_diffusion(timestep_respacing="", predict_xstart=predict_xstart,
+                                  learn_sigma=learn_sigma)  # default: 1000 steps, linear noise schedule
+
+    model.train()  # important! This enables embedding dropout for classifier-free guidance
+    
+    return model, diffusion
+
+def save_model(model, optimizer, diffusion, scheduler, n_classes, config):
+    export_dir = './models'
+
+    Path(export_dir).mkdir(parents=True, exist_ok=True)
+
+    # will save everything unless this turns out to be heavy on memory
+    checkpoint = {
+      "model": model.state_dict(),
+      "opt": optimizer.state_dict(),
+      "diffusion": diffusion,
+      "scheduler": scheduler,
+      "num_classes": n_classes,
+      "config": config,
+    }
+    exported_model_path = f"{export_dir}/predict_{'x0' if config.predict_xstart else 'noise'}_{config.epochs}.pt"
+    torch.save(checkpoint, exported_model_path)
+
+
+def load_model(model_path, device, for_training=True, return_optimizer=False):
+    state = torch.load(model_path, map_location=device)
+    
+    model = DiT(num_classes=state['num_classes']).to(device)
+    model.load_state_dict(state['model'])
+
+    optimizer = AdamW(model.parameters(), lr=1e-4, weight_decay=0)
+    optimizer.load_state_dict(state['opt'])
+
+    if not for_training:
+      model.eval()
+      return model, state['diffusion'], state['config']
+    else:
+      return model, optimizer, state['diffusion'], state['scheduler'], state['config']
+    
+def load_pretrained_autoencoder(cfg, pretrained_path=None, device=None):
+    # Load the autoencoder
+    pretrained_path = pretrained_path or "./pretrained/hierarchical_ordered.pth.tar"
+    device = device or torch.device("cuda:0"if torch.cuda.is_available() else "cpu")
+    
+    vae_model = cfg.make_model().to(device)
+    utils.load_model(pretrained_path, vae_model)
+    vae_model.eval()
+
+    return vae_model
+
+def train_with_wandb(model, train_dataloader, diffusion, optimizer, scheduler, config, device=None):    
+
+    device = device or "cuda" if torch.cuda.is_available() else "cpu"
+    steps = 0
+    
+    for epoch in range(config.epochs):
+        avg_loss = 0
+        for x, y in train_dataloader:
+            x = x.to(device)
+            y = y.to(device)
+    
+            x = x.squeeze().unsqueeze(dim=1)
+            x = x / config.magical_number # mean of std's of latents
+    
+            model_kwargs = dict(y=y)
+    
+            t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
+    
+            loss_dict = diffusion.training_losses(model, x, t, model_kwargs)
+            loss = loss_dict["loss"].mean()
+    
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            steps += 1
+            
+            wandb.log({"batch_loss": loss.item()}, step=steps)  
+            avg_loss += loss.item()
+                    
+        if config.use_scheduler: scheduler.step(avg_loss / len(train_dataloader))
+        wandb.log({"epoch": epoch, "loss": avg_loss / len(train_dataloader), 
+                   "learning_rate": optimizer.param_groups[0]['lr']})
+        
+
+def train(model, train_dataloader, diffusion, optimizer, scheduler, config, device=None):
+    device = device or "cuda" if torch.cuda.is_available() else "cpu"
+
+    for epoch in range(config.epochs):
+        avg_loss = 0
+        for x, y in train_dataloader:
+            x = x.to(device)
+            y = y.to(device)
+
+            x = x.squeeze().unsqueeze(dim=1)
+            x = x / config.magical_number # mean of std's of latents
+
+            model_kwargs = dict(y=y)
+
+            t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
+
+            loss_dict = diffusion.training_losses(model, x, t, model_kwargs)
+            loss = loss_dict["loss"].mean()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            avg_loss += loss.item()
+
+        if config.use_scheduler: scheduler.step(avg_loss / len(train_dataloader))
+        print(optimizer.param_groups[0]['lr'])
+        log_training(epoch, avg_loss / len(train_dataloader))
